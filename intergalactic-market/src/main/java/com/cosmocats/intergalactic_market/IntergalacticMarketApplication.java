3a25721b4bdc4bf14e999162ba26c47358bde967
Dana Litvak <dlitvakk21@gmail.com>
2025-11-01 20:38:34 +0200

    product api specification & project setup

---
+++ intergalactic-market/src/main/java/com/cosmocats/intergalactic_market/IntergalacticMarketApplication.java
@@ -0,0 +1,13 @@
+package com.cosmocats.intergalactic_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergalacticMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntergalacticMarketApplication.class, args);
+	}
+
+}
