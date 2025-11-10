6dade20092713f23a250d5b49b04ccccfa41742f
Dana Litvak <dlitvakk21@gmail.com>
2025-11-10 14:46:41 +0200

    add gradle extensions

--- intergalactic-market/build.gradle.kts
+++ intergalactic-market/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.7"
 	id("io.spring.dependency-management") version "1.1.7"
+	id ("org.barfuin.gradle.jacocolog") version "3.1.0"
 }
 
 group = "com.cosmocats"
@@ -14,25 +15,46 @@ java {
 	}
 }
 
+val minimumCoveragePerFile by extra(0.8)
+val filesExcludedFromCoverage by extra(listOf(
+	"**/*MarketApplication.*",
+	"**/config/*Configuration.*"
+))
+
+configurations {
+	val compileOnly by getting {
+		extendsFrom(annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
+
+apply(from = "${rootProject.projectDir}/gradle/test.gradle")
+apply(from = "${rootProject.projectDir}/gradle/jacoco.gradle")
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("jakarta.validation:jakarta.validation-api:3.1.0")
-	implementation("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+	// Lombok, MapStruct, and their binding
 	implementation("org.mapstruct:mapstruct:1.6.2")
+	compileOnly("org.projectlombok:lombok:1.18.34")
+	implementation("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+	annotationProcessor("org.projectlombok:lombok:1.18.34")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.2")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+	// Data, DB, and Migration
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.liquibase:liquibase-core:4.30.0")
 	implementation("org.postgresql:postgresql:42.7.4")
-	compileOnly("org.projectlombok:lombok:1.18.34")
-	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.2")
-	annotationProcessor("org.projectlombok:lombok:1.18.34")
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.4")
+
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// Testing
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.springframework.security:spring-security-test")
@@ -40,6 +62,8 @@ dependencies {
 	testImplementation("org.assertj:assertj-core:3.24.2")
 	testImplementation("org.wiremock:wiremock-jetty12:3.9.2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// Test Annotation Processors
 	testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
 	testAnnotationProcessor("org.mapstruct:mapstruct-processor:1.6.2")
 	testImplementation("org.projectlombok:lombok:1.18.34")
